eac27a42384ad62b898b71394edb925328d23721
Frederick Gnodtke <frederick@gnodtke.net>
2018-01-04 08:59:54 +0100

    Remove expo

---
+++ android/app/src/main/java/com/go3/MainActivity.java
@@ -0,0 +1,10 @@
+package com.go3;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "Go3";
+    }
+}
