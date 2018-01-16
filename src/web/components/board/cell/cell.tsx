8fa936eaff55921e8d7a649a196345e67b1968ab
Frederick Gnodtke <frederick@gnodtke.net>
2018-01-16 14:48:56 +0100

    Add new rendering strategy

--- src/web/components/board/cell/cell.tsx
+++ src/web/components/board/cell/cell.tsx
@@ -9,6 +9,8 @@ import { GamesStore, LoginStore } from "../../../../common-ui";
 import { Game, Color } from "../../../../common";
 import * as css from "./cell.scss";
 import { Assets } from "../../../utils";
+
+import { drawToken } from "./draw-token";
 import * as tokenBlack from "./token-black.png";
 import * as tokenWhite from "./token-white.png";
 import * as tokenInvalid from "./token-invalid.png";
@@ -37,10 +39,48 @@ export class Cell extends React.Component<CellProps> {
     @computed private get ownColor() { return this.props.game.getColorForUser(this.login.userId); }
     @computed private get isLastTurn() { return this.props.game.currentBoard.placedAt === this.props.index; }
 
+    @bind private closedTop(color: Color) {
+        const { game, index } = this.props;
+        const { col, row } = game.currentBoard.toPos(index);
+        if (row === 0) { return true; }
+        return game.currentBoard.at(game.currentBoard.toIndex({ col, row: row - 1 })) === color;
+    }
+
+    @bind private closedBottom(color: Color) {
+        const { game, index } = this.props;
+        const { col, row } = game.currentBoard.toPos(index);
+        if (row === game.boardSize - 1) { return true; }
+        return game.currentBoard.at(game.currentBoard.toIndex({ col, row: row + 1 })) === color;
+    }
+
+    @bind private closedLeft(color: Color) {
+        const { game, index } = this.props;
+        const { col, row } = game.currentBoard.toPos(index);
+        if (col === 0) { return true; }
+        return game.currentBoard.at(game.currentBoard.toIndex({ col: col - 1, row })) === color;
+    }
+
+    @bind private closedRight(color: Color) {
+        const { game, index } = this.props;
+        const { col, row } = game.currentBoard.toPos(index);
+        if (col === game.boardSize - 1) { return true; }
+        return game.currentBoard.at(game.currentBoard.toIndex({ col: col + 1, row })) === color;
+    }
+
     @computed private get valid() {
         const { game, index } = this.props;
         const errorMessage = game.turnValid(index);
         return this.login.userId === game.currentUser.id && typeof errorMessage === "undefined";
+    }
+
+    @computed private get asset() {
+        const { color, hovered, locked, ownColor } = this;
+        if (color === Color.BLACK) { return this.assets.get(tokenBlack); }
+        if (color === Color.WHITE) { return this.assets.get(tokenWhite); }
+        if (hovered || locked) {
+            if (ownColor === Color.BLACK) { return this.assets.get(tokenBlack); }
+            if (ownColor === Color.WHITE) { return this.assets.get(tokenWhite); }
+        }
     }
 
     @initialize
@@ -105,32 +145,34 @@ export class Cell extends React.Component<CellProps> {
         const { width, height } = this.canvas;
         const ctx = this.canvas.getContext("2d");
 
-        const { hovered, locked, valid, color, ownColor, isLastTurn } = this;
+        const { color, ownColor, valid, isLastTurn, asset, hovered, locked } = this;
         const { game, index } = this.props;
 
         ctx.clearRect(0, 0, width, height);
-        switch (color) {
-            case Color.BLACK:
-                ctx.drawImage(this.assets.get(tokenBlack), 0, 0, 200, 200, 0, 0, width, height);
-                break;
-            case Color.WHITE:
-                ctx.drawImage(this.assets.get(tokenWhite), 0, 0, 200, 200, 0, 0, width, height);
-                break;
-            default:
-                if (!hovered && !locked) { break; }
-                if (!valid) {
-                    ctx.drawImage(this.assets.get(tokenInvalid), 0, 0, 200, 200, 0, 0, width, height);
-                }
-                else if (ownColor === Color.BLACK) {
-                    ctx.drawImage(this.assets.get(tokenBlack), 0, 0, 200, 200, 0, 0, width, height);
-                }
-                else if (ownColor === Color.WHITE) {
-                    ctx.drawImage(this.assets.get(tokenWhite), 0, 0, 200, 200, 0, 0, width, height);
-                }
-                break;
+        if (color === Color.EMPTY && !valid && (hovered || locked)) {
+            ctx.drawImage(this.assets.get(tokenInvalid), 0, 0, 200, 200, 0, 0, width, height);
+            return;
         }
+        if (!asset) { return; }
+
+        const colors = {
+            closedTop: color === Color.EMPTY ? this.closedTop(ownColor) : this.closedTop(color),
+            closedBottom: color === Color.EMPTY ? this.closedBottom(ownColor) : this.closedBottom(color),
+            closedLeft: color === Color.EMPTY ? this.closedLeft(ownColor) : this.closedLeft(color),
+            closedRight: color === Color.EMPTY ? this.closedRight(ownColor) : this.closedRight(color),
+        };
+
+        const instructions = {
+            width,
+            height,
+            ctx,
+            asset,
+            ...colors,
+        };
+        drawToken("modern", instructions);
+
         if (isLastTurn) {
-            ctx.drawImage(this.assets.get(tokenLast), 0, 0, 200, 200, 0, 0, width, height);
+            drawToken("modern", { ... instructions, asset: this.assets.get(tokenLast) });
         }
     }
 
---
+++ src/web/components/board/cell/draw-token.ts
@@ -0,0 +1,121 @@
+interface DrawInstructions {
+    width: number;
+    height: number;
+    ctx: CanvasRenderingContext2D;
+    asset: HTMLImageElement;
+    closedTop: boolean;
+    closedBottom: boolean;
+    closedLeft: boolean;
+    closedRight: boolean;
+}
+
+const assetWidth = 200;
+const assetHeight = 200;
+
+export const strategies = new Map<string, (DrawInstructions) => void>();
+
+strategies.set("classic", (drawInstructions: DrawInstructions) => {
+    const { asset, ctx, width, height } = drawInstructions;
+    ctx.drawImage(asset, 0, 0, assetWidth, assetHeight, 0, 0, width, height);
+});
+
+strategies.set("modern", (drawInstructions: DrawInstructions) => {
+    console.log(drawInstructions)
+    const { asset, ctx, width, height, closedTop, closedBottom, closedLeft, closedRight } = drawInstructions;
+    let topLeftRound = true;
+    let topRightRound = true;
+    let bottomLeftRound = true;
+    let bottomRightRound = true;
+    if (closedTop) {
+        topLeftRound = false;
+        topRightRound = false;
+    }
+    if (closedBottom) {
+        bottomLeftRound = false;
+        bottomRightRound = false;
+    }
+    if (closedLeft) {
+        topLeftRound = false;
+        bottomLeftRound = false;
+    }
+    if (closedRight) {
+        topRightRound = false;
+        bottomRightRound = false;
+    }
+    if (topLeftRound) {
+        ctx.drawImage(
+            asset,
+            0, 0,
+            assetWidth / 2, assetHeight / 2,
+            0, 0,
+            width / 2, height / 2,
+        );
+    } else {
+        ctx.drawImage(
+            asset,
+            assetWidth, 0,
+            assetWidth / 2, assetHeight / 2,
+            0, 0,
+            width / 2, height / 2,
+        );
+    }
+    if (topRightRound) {
+        ctx.drawImage(
+            asset,
+            assetWidth / 2, 0,
+            assetWidth / 2, assetHeight / 2,
+            width / 2, 0,
+            width / 2, height / 2,
+        );
+    } else {
+        ctx.drawImage(
+            asset,
+            assetWidth + assetWidth / 2, 0,
+            assetWidth / 2, assetHeight / 2,
+            width / 2, 0,
+            width / 2, height / 2,
+        );
+    }
+    if (bottomLeftRound) {
+        ctx.drawImage(
+            asset,
+            0, assetHeight / 2,
+            assetWidth / 2, assetHeight / 2,
+            0, height / 2,
+            width / 2, height / 2,
+        );
+    } else {
+        ctx.drawImage(
+            asset,
+            assetWidth, assetHeight / 2,
+            assetWidth / 2, assetHeight / 2,
+            0, height / 2,
+            width / 2, height / 2,
+        );
+    }
+    if (bottomRightRound) {
+        ctx.drawImage(
+            asset,
+            assetWidth / 2, assetHeight / 2,
+            assetWidth / 2, assetHeight / 2,
+            width / 2, height / 2,
+            width / 2, height / 2,
+        );
+    } else {
+        ctx.drawImage(
+            asset,
+            assetWidth + assetWidth / 2, assetHeight / 2,
+            assetWidth / 2, assetHeight / 2,
+            width / 2, height / 2,
+            width / 2, height / 2,
+        );
+    }
+});
+
+export function drawToken(strategy: string, instructions: DrawInstructions) {
+    if (!strategies.has(strategy)) {
+        return;
+    }
+    // console.log(strategies.get(strategy))
+    strategies.get(strategy)(instructions);
+}
