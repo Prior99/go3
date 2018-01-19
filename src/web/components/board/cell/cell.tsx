8f1d3dd9ec292bb7018c0c3df719648b357e1922
Frederick Gnodtke <frederick@gnodtke.net>
2018-01-19 22:37:06 +0100

    Implement lining based on group status

--- src/web/components/board/cell/cell.tsx
+++ src/web/components/board/cell/cell.tsx
@@ -6,9 +6,9 @@ import { inject, external, initialize } from "tsdi";
 import { bind } from "decko";
 
 import { GamesStore, LoginStore, OwnUserStore } from "../../../../common-ui";
-import { Game, Color } from "../../../../common";
+import { Game, Color, Group } from "../../../../common";
 import * as css from "./cell.scss";
-import { Assets, drawToken, Asset } from "../../../utils";
+import { Assets, drawToken } from "../../../utils";
 
 import * as tokenInvalid from "./token-invalid.png";
 
@@ -40,6 +40,34 @@ export class Cell extends React.Component<CellProps> {
     @computed private get color() { return this.props.game.currentBoard.at(this.props.index); }
     @computed private get ownColor() { return this.props.game.getColorForUser(this.login.userId); }
     @computed private get isLastTurn() { return this.props.game.currentBoard.placedAt === this.props.index; }
+
+    @bind private closedTopLeft(color: Color) {
+        const { game, index } = this.props;
+        const { col, row } = game.currentBoard.toPos(index);
+        if (col === 0 || row === 0) { return true; }
+        return game.currentBoard.at(game.currentBoard.toIndex({ col: col - 1, row: row - 1 })) === color;
+    }
+
+    @bind private closedBottomLeft(color: Color) {
+        const { game, index } = this.props;
+        const { col, row } = game.currentBoard.toPos(index);
+        if (col === 0 || row === game.boardSize - 1) { return true; }
+        return game.currentBoard.at(game.currentBoard.toIndex({ col: col - 1, row: row + 1 })) === color;
+    }
+
+    @bind private closedTopRight(color: Color) {
+        const { game, index } = this.props;
+        const { col, row } = game.currentBoard.toPos(index);
+        if (col === game.boardSize - 1 || row === 0) { return true; }
+        return game.currentBoard.at(game.currentBoard.toIndex({ col: col + 1, row: row - 1 })) === color;
+    }
+
+    @bind private closedBottomRight(color: Color) {
+        const { game, index } = this.props;
+        const { col, row } = game.currentBoard.toPos(index);
+        if (col === game.boardSize - 1 || row === game.boardSize - 1) { return true; }
+        return game.currentBoard.at(game.currentBoard.toIndex({ col: col + 1, row: row + 1 })) === color;
+    }
 
     @bind private closedTop(color: Color) {
         const { game, index } = this.props;
@@ -75,14 +103,17 @@ export class Cell extends React.Component<CellProps> {
         return this.login.userId === game.currentUser.id && typeof errorMessage === "undefined";
     }
 
-    @computed private get asset() {
+    @computed private get renderedColor() {
         const { color, hovered, locked, ownColor } = this;
-        if (color === Color.BLACK) { return Asset.BLACK; }
-        if (color === Color.WHITE) { return Asset.WHITE; }
+        if (color !== Color.EMPTY) { return color; }
         if (hovered || locked) {
-            if (ownColor === Color.BLACK) { return Asset.BLACK; }
-            if (ownColor === Color.WHITE) { return Asset.WHITE; }
+            return ownColor;
         }
+        return Color.EMPTY;
+    }
+
+    @computed private get group(): Group {
+        return this.props.game.currentBoard.groupAt(this.props.index);
     }
 
     @bind private async initCanvas() {
@@ -155,7 +186,7 @@ export class Cell extends React.Component<CellProps> {
 
         (ctx as any).imageSmoothingEnabled = "high";
 
-        const { color, ownColor, valid, isLastTurn, asset, hovered, locked } = this;
+        const { color, ownColor, valid, isLastTurn, renderedColor, hovered, locked, group } = this;
         const { game, index } = this.props;
 
         ctx.clearRect(0, 0, width, height);
@@ -163,26 +194,32 @@ export class Cell extends React.Component<CellProps> {
             ctx.drawImage(this.assets.get(tokenInvalid, width, height), 0, 0, width, height, 0, 0, width, height);
             return;
         }
-        if (!asset) { return; }
 
         const colors = {
             closedTop: color === Color.EMPTY ? this.closedTop(ownColor) : this.closedTop(color),
             closedBottom: color === Color.EMPTY ? this.closedBottom(ownColor) : this.closedBottom(color),
             closedLeft: color === Color.EMPTY ? this.closedLeft(ownColor) : this.closedLeft(color),
             closedRight: color === Color.EMPTY ? this.closedRight(ownColor) : this.closedRight(color),
+            closedTopLeft: color === Color.EMPTY ? this.closedTopLeft(ownColor) : this.closedTopLeft(color),
+            closedTopRight: color === Color.EMPTY ? this.closedTopRight(ownColor) : this.closedTopRight(color),
+            closedBottomRight: color === Color.EMPTY ? this.closedBottomRight(ownColor) : this.closedBottomRight(color),
+            closedBottomLeft: color === Color.EMPTY ? this.closedBottomLeft(ownColor) : this.closedBottomLeft(color),
         };
 
         const instructions = {
             width,
             height,
             ctx,
-            asset,
+            color: renderedColor,
+            last: false,
             ...colors,
+            preview: color === Color.EMPTY && (locked || hovered),
+            status: group.status,
         };
         drawToken(this.ownUser.user.renderingStrategy, instructions);
 
         if (isLastTurn) {
-            drawToken(this.ownUser.user.renderingStrategy, { ...instructions, asset: Asset.LAST });
+            drawToken(this.ownUser.user.renderingStrategy, { ...instructions, last: true });
         }
     }
 
--- src/web/utils/rendering-strategy.ts
+++ src/web/utils/rendering-strategy.ts
@@ -1,61 +1,86 @@
-import { RenderingStrategy } from "../../common";
+import { RenderingStrategy, Color, GroupStatus } from "../../common";
 
-export enum Asset {
-    BLACK = "black",
-    WHITE = "white",
-    LAST = "last",
-}
+const colors = {
+    color: {
+        [Color.WHITE]: "rgb(220, 220, 220)",
+        [Color.BLACK]: "rgb(20, 20, 20)",
+    },
+    last: "rgb(80, 80, 255)",
+    status: {
+        [GroupStatus.ALIVE]: "rgb(80, 255, 80)",
+        [GroupStatus.UNDECIDED]: "rgb(255, 255, 80)",
+        [GroupStatus.DEAD]: "rgb(255, 80, 80)",
+    },
+};
 
 interface DrawInstructions {
     width: number;
     height: number;
     ctx: CanvasRenderingContext2D;
-    asset: Asset;
+    color: Color;
     closedTop: boolean;
     closedBottom: boolean;
     closedLeft: boolean;
     closedRight: boolean;
+    closedTopLeft: boolean;
+    closedBottomLeft: boolean;
+    closedTopRight: boolean;
+    closedBottomRight: boolean;
+    last: boolean;
+    preview: boolean;
+    status: GroupStatus;
 }
 
 export const strategies = new Map<RenderingStrategy, (DrawInstructions) => void>();
 
 strategies.set(RenderingStrategy.CLASSIC, (drawInstructions: DrawInstructions) => {
-    const { asset, ctx, width, height } = drawInstructions;
+    const { color, ctx, width, height, last, preview, status } = drawInstructions;
 
-    switch (asset) {
-        case Asset.WHITE:
-            ctx.fillStyle = "rgb(220, 220, 220)";
+    ctx.fillStyle = colors.color[color];
+    switch (color) {
+        case Color.WHITE:
             ctx.beginPath();
             ctx.arc(width / 2, height / 2, width / 2 - 1, 0, Math.PI * 2);
             ctx.fill();
             break;
-        case Asset.BLACK:
-            ctx.fillStyle = "rgb(20, 20, 20)";
+        case Color.BLACK:
             ctx.beginPath();
             ctx.arc(width / 2, height / 2, width / 2 - 1, 0, Math.PI * 2);
             ctx.fill();
-            break;
-        case Asset.LAST:
-            ctx.strokeStyle = "rgb(255, 80, 0)";
-            ctx.lineWidth = 4;
-            ctx.beginPath();
-            ctx.arc(width / 2, height / 2, width / 2 - 2, 0, Math.PI * 2);
-            ctx.stroke();
             break;
         default: break;
     }
+
+    if (last) {
+        ctx.strokeStyle = colors.last;
+        ctx.lineWidth = 4;
+        ctx.beginPath();
+        ctx.arc(width / 2 - 1, height / 2 - 1, width / 2 - 15, 0, Math.PI * 2);
+        ctx.stroke();
+    }
+    if (!preview && color !== Color.EMPTY) {
+        ctx.strokeStyle = colors.status[status];
+        ctx.lineWidth = 4;
+        ctx.beginPath();
+        ctx.arc(width / 2, height / 2, width / 2 - 2, 0, Math.PI * 2);
+        ctx.stroke();
+    }
 });
 
-strategies.set(RenderingStrategy.MODERN, (drawInstructions: DrawInstructions) => {
-    const { asset, ctx, width, height, closedTop, closedBottom, closedLeft, closedRight } = drawInstructions;
+interface RoundResult {
+    topLeftRound: boolean;
+    topRightRound: boolean;
+    bottomLeftRound: boolean;
+    bottomRightRound: boolean;
+}
 
-    const assetWidth = width;
-    const assetHeight = height;
-
+function getRounds(drawInstructions: DrawInstructions): RoundResult {
+    const { closedTop, closedBottom, closedLeft, closedRight } = drawInstructions;
     let topLeftRound = true;
     let topRightRound = true;
     let bottomLeftRound = true;
     let bottomRightRound = true;
+
     if (closedTop) {
         topLeftRound = false;
         topRightRound = false;
@@ -72,82 +97,195 @@ strategies.set(RenderingStrategy.MODERN, (drawInstructions: DrawInstructions) =>
         topRightRound = false;
         bottomRightRound = false;
     }
-    const border = asset === Asset.LAST ? 2 : 0;
+
+    return {
+        topLeftRound,
+        topRightRound,
+        bottomLeftRound,
+        bottomRightRound,
+    };
+}
+
+function drawLast(drawInstructions: DrawInstructions) {
+    const { width, height, ctx, last } = drawInstructions;
+    const { topLeftRound, topRightRound, bottomLeftRound, bottomRightRound } = getRounds(drawInstructions);
+
+    if (!last) { return; }
+
+    const offset = 15;
+    const radius = width / 2 - offset;
+
+    ctx.beginPath();
+    ctx.moveTo(offset, height / 2);
+    if (topLeftRound) {
+        ctx.arc(width / 2, height / 2, radius, Math.PI, Math.PI * 1.5);
+    } else {
+        ctx.lineTo(offset, offset);
+        ctx.lineTo(width / 2, offset);
+    }
+    ctx.moveTo(width / 2, offset);
+    if (topRightRound) {
+        ctx.arc(width / 2, height / 2, radius, Math.PI * 1.5, Math.PI * 2);
+    } else {
+        ctx.lineTo(width - offset, offset);
+        ctx.lineTo(width - offset, height / 2);
+    }
+    ctx.moveTo(width - offset, height / 2);
+    if (bottomRightRound) {
+        ctx.arc(width / 2, height / 2, radius, 0, Math.PI * 0.5);
+    } else {
+        ctx.lineTo(width - offset, height - offset);
+        ctx.lineTo(width / 2, height - offset);
+    }
+    ctx.moveTo(width / 2, height - offset);
+    if (bottomLeftRound) {
+        ctx.arc(width / 2, height / 2, radius, Math.PI * 0.5, Math.PI);
+    } else {
+        ctx.lineTo(offset, height - offset);
+        ctx.lineTo(offset, height / 2);
+    }
+
+    ctx.strokeStyle = colors.last;
+    ctx.lineWidth = 4;
+    ctx.stroke();
+}
+
+function drawLining(drawInstructions: DrawInstructions) {
+    const {
+        width,
+        height,
+        ctx,
+        color,
+        closedRight,
+        closedTop,
+        closedLeft,
+        closedBottom,
+        closedTopRight,
+        closedTopLeft,
+        closedBottomLeft,
+        closedBottomRight,
+        preview,
+        status,
+    } = drawInstructions;
+    const { topLeftRound, topRightRound, bottomLeftRound, bottomRightRound } = getRounds(drawInstructions);
+
+    if (color === Color.EMPTY || preview) { return; }
+
+    const border = 2;
     const radius = width / 2 - border;
-    const stroke = asset === Asset.LAST;
 
     ctx.beginPath();
     ctx.moveTo(border, height / 2);
+    ctx.strokeStyle = colors.status[status];
+    ctx.fillStyle = ctx.strokeStyle;
+    ctx.lineWidth = 4;
+
     if (topLeftRound) {
-        if (!stroke) { ctx.moveTo(width / 2, height / 2); }
         ctx.arc(width / 2, height / 2, radius, Math.PI, Math.PI * 1.5);
     } else {
-        if (stroke) {
-            ctx.lineTo(border, border);
-            ctx.lineTo(width / 2, border);
-        } else {
-            ctx.rect(0, 0, width / 2, height / 2);
-        }
+        if (!closedLeft) { ctx.lineTo(border, 0); }
+        ctx.moveTo(0, border);
+        if (!closedTop) { ctx.lineTo(width / 2, border); }
+        ctx.moveTo(width / 2, border);
     }
     ctx.moveTo(width / 2, border);
     if (topRightRound) {
-        if (!stroke) { ctx.moveTo(width / 2, height / 2); }
         ctx.arc(width / 2, height / 2, radius, Math.PI * 1.5, Math.PI * 2);
     } else {
-        if (stroke) {
-            ctx.lineTo(width - border, border);
-            ctx.lineTo(width - border, height / 2);
-        } else {
-            ctx.rect(width / 2, 0, width / 2, height / 2);
-        }
+        if (!closedTop) { ctx.lineTo(width, border); }
+        ctx.moveTo(width - border, 0);
+        if (!closedRight) { ctx.lineTo(width - border, height / 2); }
+        ctx.moveTo(width - border, height / 2);
     }
     ctx.moveTo(width - border, height / 2);
     if (bottomRightRound) {
-        if (!stroke) { ctx.moveTo(width / 2, height / 2); }
         ctx.arc(width / 2, height / 2, radius, 0, Math.PI * 0.5);
     } else {
-        if (stroke) {
-            ctx.lineTo(width - border, height - border);
-            ctx.lineTo(width / 2, height - border);
-        } else {
-            ctx.rect(width / 2, height / 2, width / 2, height / 2);
-        }
+        if (!closedRight) { ctx.lineTo(width - border, height); }
+        ctx.moveTo(width, height - border);
+        if (!closedBottom) { ctx.lineTo(width / 2, height - border); }
+        ctx.moveTo(width / 2, height - border);
     }
     ctx.moveTo(width / 2, height - border);
     if (bottomLeftRound) {
-        if (!stroke) { ctx.moveTo(width / 2, height / 2); }
         ctx.arc(width / 2, height / 2, radius, Math.PI * 0.5, Math.PI);
     } else {
-        if (stroke) {
-            ctx.lineTo(border, height - border);
-            ctx.lineTo(border, height / 2);
-        } else {
-            ctx.rect(0, height / 2, width / 2, height / 2);
-        }
+        if (!closedBottom) { ctx.lineTo(0, height - border); }
+        ctx.moveTo(border, height);
+        if (!closedLeft) { ctx.lineTo(border, height / 2); }
+        ctx.moveTo(border, height / 2);
     }
 
-    switch (asset) {
-        case Asset.WHITE:
+    ctx.lineWidth = 4;
+    ctx.stroke();
+
+    if (!topLeftRound && !closedTopLeft) {
+        ctx.fillRect(0, 0, 4, 4);
+    }
+    if (!topRightRound && !closedTopRight) {
+        ctx.fillRect(width - 4, 0, 4, 4);
+    }
+    if (!bottomRightRound && !closedBottomRight) {
+        ctx.fillRect(width - 4, height - 4, 4, 4);
+    }
+    if (!bottomLeftRound && !closedBottomLeft) {
+        ctx.fillRect(0, height - 4, 4, 4);
+    }
+}
+
+strategies.set(RenderingStrategy.MODERN, (drawInstructions: DrawInstructions) => {
+    const { color, last, ctx, width, height, closedTop, closedBottom, closedLeft, closedRight } = drawInstructions;
+    const { topLeftRound, topRightRound, bottomLeftRound, bottomRightRound } = getRounds(drawInstructions);
+
+    ctx.beginPath();
+    ctx.moveTo(0, height / 2);
+    if (topLeftRound) {
+        ctx.moveTo(width / 2, height / 2);
+        ctx.arc(width / 2, height / 2, width / 2, Math.PI, Math.PI * 1.5);
+    } else {
+        ctx.rect(0, 0, width / 2, height / 2);
+    }
+    ctx.moveTo(width / 2, 0);
+    if (topRightRound) {
+        ctx.moveTo(width / 2, height / 2);
+        ctx.arc(width / 2, height / 2, width / 2, Math.PI * 1.5, Math.PI * 2);
+    } else {
+        ctx.rect(width / 2, 0, width / 2, height / 2);
+    }
+    ctx.moveTo(width - 0, height / 2);
+    if (bottomRightRound) {
+        ctx.moveTo(width / 2, height / 2);
+        ctx.arc(width / 2, height / 2, width / 2, 0, Math.PI * 0.5);
+    } else {
+        ctx.rect(width / 2, height / 2, width / 2, height / 2);
+    }
+    ctx.moveTo(width / 2, height - 0);
+    if (bottomLeftRound) {
+        ctx.moveTo(width / 2, height / 2);
+        ctx.arc(width / 2, height / 2, width / 2, Math.PI * 0.5, Math.PI);
+    } else {
+        ctx.rect(0, height / 2, width / 2, height / 2);
+    }
+
+    switch (color) {
+        case Color.WHITE:
             ctx.fillStyle = "rgb(220, 220, 220)";
             ctx.fill();
             break;
-        case Asset.BLACK:
+        case Color.BLACK:
             ctx.fillStyle = "rgb(20, 20, 20)";
             ctx.fill();
             break;
-        case Asset.LAST:
-            ctx.strokeStyle = "rgb(255, 80, 0)";
-            ctx.lineWidth = 4;
-            ctx.stroke();
-            break;
         default: break;
     }
+
+    drawLast(drawInstructions);
+    drawLining(drawInstructions);
 });
 
 export function drawToken(strategy: RenderingStrategy, instructions: DrawInstructions) {
     if (!strategies.has(strategy)) {
         return;
     }
-    // console.log(strategies.get(strategy))
     strategies.get(strategy)(instructions);
 }
